3bd9ac0f8ca18b2698db2ebd2b2101e9217351e0
Pierre Vaidie <pierre.vaidie@gmail.com>
2012-11-28 14:36:11 +0100

    Initial Import

---
+++ src/Mouf/Html/Utils/I18nWebLibrary/I18nWebLibrary.php
@@ -0,0 +1,33 @@
+<?php
+namespace Mouf\Html\Utils\I18nWebLibrary;
+use Mouf\Html\Utils\WebLibraryManager;
+/**
+ * @Component
+ * @author Kevin
+ *
+ */
+class I18nWebLibrary extends WebLibrary{
+	
+	/**
+	 * @Property
+	 * @var LanguageDetectionInterface
+	 */
+	public $languageDetection;
+	
+	/**
+	 * (non-PHPdoc)
+	 * Override the parent "getJSFiles"', by replacing the [lang] occurences depending on the detected Language
+	 * @see WebLibrary::getJsFiles()
+	 */
+	public function getJsFiles(){
+		$lang = $this->languageDetection->getLanguage();
+		
+		$files = array();
+		foreach (parent::getJsFiles() as $file) {
+			$files[] = str_replace("[lang]", $lang, $file);
+		}
+		
+		return $files;
+	}
+	
+}
